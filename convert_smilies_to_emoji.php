8e1c980acd73a5a6f259a18b3b51e1617af0101c
terry chay <tychay@php.net>
2015-04-26 06:53:14 -0700

    initial commit
    From pluginception + gitignore
    
    Signed-off-by: terry chay <tychay@php.net>

---
+++ convert_smilies_to_emoji.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: convert smilies… to emoji
+Plugin URI: http://github.com/tychay/
+Description: Replace convert_smilies() images with emoji
+Version: 1.0
+Author: terry chay
+Author URI: http://terrychay.com
+License: GPL v2.0 or newer
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+*/
